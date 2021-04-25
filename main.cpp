b7a7eda233f36db4615eb90fc23e02431e405f38
nigroborhin <nigroborgin@yandex.ru>
2021-04-25 23:46:04 +0300

    Bugfix

--- main.cpp
+++ main.cpp
@@ -4,9 +4,20 @@
 using namespace std;
 
 int main() {
-  cout << "Time test:\n";
-  string path = "E:\\CppProjects\\semester-work-b-tree-11-004\\dataset\\data\\insert\\data(10000).txt";
 
-  string a = Parser::process_data(path);
+  string pathToTests;
+  string pathToResult;
+  int iterations = 1;
+
+
+  cout << "Enter path to file with tests:\n";
+  cin >> pathToTests;
+  cout << "Enter path to results file:\n";
+  cin >> pathToResult;
+
+  cout << "Enter iterations:\n";
+  cin >> iterations;
+
+  Parser::process_data(pathToTests, pathToResult, iterations);
   return 0;
 }
